105ed12a77222ef2b53e22422facbdf1c25b8276
Urusha Shaikh <urusha.s@thinkbridge.in>
2024-03-24 23:04:27 +0530

    Changes done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './core/components/navbar/navbar.component';
+import { RouterModule, Routes } from '@angular/router';
+import { UserDetailsComponent } from './features/users/user-details/user-details.component';
+
+const routes: Routes = [
+  { path: 'user-details', component: UserDetailsComponent } // Define routes
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    UserDetailsComponent
+  
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
